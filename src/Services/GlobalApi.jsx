5a7cdec7c9ac737a00a1e48355e343cfb52b5bea
Juan11211 <avilesjuan628@gmail.com>
2023-10-03 07:43:34 -0400

    wow

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,33 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
-const movieBaseUrl = 'https://api.themoviedb.org/3';
-const api_key = 'a8d225842e59d3766ab358c78b2670c0'
+const movieBaseUrl="https://api.themoviedb.org/3"
+const api_key='a8d225842e59d3766ab358c78b2670c0'
 
-const trendingVideosUrl = `${movieBaseUrl}/trending/all/day?api_key=${api_key}`;
-const movieByGenreBaseURL = `${movieBaseUrl}/discover/movie?api_key=${api_key}`;
+const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
 
-const getTrendingVideos = async () => {
-  try {
-    const response = await axios.get(trendingVideosUrl);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching trending videos:', error);
-    throw error;
-  }
-};
+//https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
+const getTrending=axios.get(movieBaseUrl+
+    "/trending/all/day?api_key="+api_key);
+    const getMovieByGenreId=(id)=>
+    axios.get(movieByGenreBaseURL+"&with_genres="+id)
 
-const getMovieByGenreId = async (id) => {
-  try {
-    const response = await axios.get(`${movieByGenreBaseURL}&with_genres=${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching movies by genre:', error);
-    throw error;
-  }
-};
-
-export default {
-  getTrendingVideos,
-  getMovieByGenreId,
-};
-
+export default{
+    getTrending,
+    getMovieByGenreId
+}
