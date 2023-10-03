9d18a1f1385126f0b5f1120484840bddc20eec01
Juan11211 <avilesjuan628@gmail.com>
2023-10-03 15:07:42 -0400

    got the slider working, need to fix movieList

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -6,12 +6,15 @@ const api_key='a8d225842e59d3766ab358c78b2670c0'
 const movieByGenreBaseURL='https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf';
 
 //https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
-const getTrending=axios.get(movieBaseUrl+
-    "/trending/all/day?api_key="+api_key);
-    const getMovieByGenreId=(id)=>
-    axios.get(movieByGenreBaseURL+"&with_genres="+id)
-
-export default{
-    getTrending,
+const getTrendingVideos = () => {
+    return axios.get(movieBaseUrl + "/trending/all/day?api_key=" + api_key);
+  };
+  
+  const getMovieByGenreId = (id) => {
+    return axios.get(movieBaseUrl + "/discover/movie?api_key=" + api_key + "&with_genres=" + id);
+  };
+  
+  export default {
+    getTrendingVideos,
     getMovieByGenreId
-}
+  };
